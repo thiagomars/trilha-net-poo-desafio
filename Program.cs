408b6145f6955ee1774693b24be9179518781cbb
thiagomars <r15.thiagomaques@alu.ufc.br>
2023-01-17 16:01:22 -0300

    curso finalizado

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "Modelo 1", imei: "1111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone");
+Smartphone iPhone = new Iphone(numero: "67890", modelo: "Modelo 2", imei: "2222222222", memoria: 128);
+iPhone.Ligar();
+iPhone.InstalarAplicativo("Telegram");
